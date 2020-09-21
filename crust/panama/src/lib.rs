243bf4d00a0a87080f8ef4a9ea3ab452fd0d1ded
Tiago Chaves <tiagoloureirochaves@gmail.com>
2020-09-21 08:25:43 -0300

    Fix hanging test

--- crust/panama/src/lib.rs
+++ crust/panama/src/lib.rs
@@ -1,5 +1,3 @@
-// @Todo: continue from https://youtu.be/b4mS5UPHh20?t=2659
-
 use std::collections::VecDeque;
 use std::sync::{Arc, Condvar, Mutex};
 
@@ -18,9 +16,10 @@ pub fn channel<T>() -> (Sender<T>, Receiver<T>) {
     let inner = Inner { queue: VecDeque::default(), senders: 1 };
     let shared = Shared { inner: Mutex::new(inner), available: Condvar::new() };
     let shared = Arc::new(shared);
+
     (
         Sender { shared: Arc::clone(&shared) },
-        Receiver { shared: Arc::clone(&shared) }
+        Receiver { shared: Arc::clone(&shared) },
     )
 }
 
@@ -55,7 +54,9 @@ impl<T> Drop for Sender<T> {
         let was_last = inner.senders == 0;
         drop(inner);
         if was_last {
-            self.shared.available.notify_one() // @Fixme: shouldn't this be `notify_all()`?
+            // @Note: when the last sender goes away, there can only be the
+            // receiver left. So, there's at most *one* thread waiting.
+            self.shared.available.notify_one()
         }
     }
 }
@@ -81,18 +82,30 @@ pub struct Receiver<T> {
 }
 
 impl<T> Receiver<T> {
-    pub fn recv(&mut self) -> T {
+    pub fn recv(&mut self) -> Option<T> {
         let mut inner = self.shared.inner.lock().unwrap();
         // @Note: this loop is not a spinlock because, using `Condvar`, we can block a
         // thread such that it consumes no CPU time while waiting for an event to occur.
         loop {
             match inner.queue.pop_front() {
-                Some(t) => return t,
+                Some(t) => return Some(t),
                 None => {
-                    inner = self.shared.available.wait(inner).unwrap();
+                    if inner.senders == 0 {
+                        return None;
+                    } else {
+                        inner = self.shared.available.wait(inner).unwrap();
+                    }
                 }
             }
         }
+    }
+}
+
+impl<T> Iterator for Receiver<T> {
+    type Item = T;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        self.recv()
     }
 }
 
@@ -108,13 +121,28 @@ mod tests {
     fn ping_pong() {
         let (mut tx, mut rx) = channel();
         tx.send(42);
-        assert_eq!(rx.recv(), 42);
+        assert_eq!(rx.recv(), Some(42));
     }
 
     #[test]
-    fn closed() {
+    fn closed_tx() {
         let (tx, mut rx) = channel::<()>();
-        let _ = tx;
-        let _ = rx.recv();
+
+        // @Note: assigning to underscore doesn't immediately drop.
+        // So if we used this, instead of `drop()`, it'd hang forever:
+        //  |
+        //  |   let _ = tx;
+        //
+        drop(tx);
+        assert_eq!(rx.recv(), None);
+    }
+
+    #[test]
+    fn closed_rx() {
+        let (mut tx, rx) = channel();
+
+        // See https://youtu.be/b4mS5UPHh20?t=3282
+        drop(rx);
+        tx.send(42);
     }
 }
